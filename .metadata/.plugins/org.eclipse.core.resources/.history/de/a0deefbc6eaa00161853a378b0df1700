5ed8153c87a1a20bac082cc4e8ac556ff381d892
ytelloUTC <elizatello39@gmail.com>
2016-11-14 09:58:55 -0500

    ytelloUTCGrupoC6

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/de/a0deefbc6eaa00161853a378b0df1700
@@ -0,0 +1,22 @@
+package models;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import play.data.validation.Constraints;
+
+@Entity
+public class Disco extends com.avaje.ebean.Model {
+	
+	
+private static final long serialVersionUID= 1L;
+@Id
+public Long codigo;
+@Constraints.Required
+public String marca;
+@Constraints.Required
+public int capacidad;
+@Constraints.Required
+public int nucleos;
+
+}
--- play-ebean-example-master/app/models/Disco.java
+++ play-ebean-example-master/app/models/Disco.java
@@ -2,21 +2,34 @@ package models;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+import com.avaje.ebean.Model;
+import com.avaje.ebean.PagedList;
+import com.avaje.ebean.Model.Find;
 
 import play.data.validation.Constraints;
 
 @Entity
-public class Disco extends com.avaje.ebean.Model {
+public class Disco extends Model {
 	
-	
-private static final long serialVersionUID= 1L;
+	 private static final long serialVersiongID = 1L;
+
+
 @Id
-public Long codigo;
+public Long id;
+
 @Constraints.Required
-public String marca;
+public String name;
+
 @Constraints.Required
 public int capacidad;
+
 @Constraints.Required
 public int nucleos;
+@OneToOne
+public Computer computer;
+
+public static Find<Long,Disco> find = new Find<Long,Disco>(){};
 
 }
